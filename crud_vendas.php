f5bef0451bbd901c53c3645aec34c40a21a40ba0
matos-gabriel <matosgabriel017@gmail.com>
2020-10-08 11:25:30 -0300

    av2 - final part1

--- crud_vendas.php
+++ crud_vendas.php
@@ -5,66 +5,128 @@ session_start();
 include('conexao.php');
 
 # inicializa variáveis
-$nomecli = "";
-$endercli = "";
-$fonecli = "";
-$emailcli = "";
-$idcli = 0;
+$idcli = "";
+$codven = 0;
+$idprod = "";
+$idcli = "";
+$qtdven = "";
 $update = false;
 
+//testes
+$hasStock = false;
+$existCli = "";
+$existProd = "";
 # adiciona produto
 if (isset($_POST['adiciona'])) {
-    $nomecli = filter_input(INPUT_POST, 'nomecli');
-    $endercli = filter_input(INPUT_POST, 'endercli');
-    $fonecli = filter_input(INPUT_POST, 'fonecli');
-    $emailcli = filter_input(INPUT_POST, 'emailcli',FILTER_VALIDATE_EMAIL);
+    $idcli = filter_input(INPUT_POST, 'idcli', FILTER_VALIDATE_INT);
+    $idprod = filter_input(INPUT_POST, 'idprod', FILTER_VALIDATE_INT);
+    $qtdven = filter_input(INPUT_POST, 'qtdven', FILTER_VALIDATE_INT);
 
+    if ($idprod && $idcli && $qtdven) {
 
-    if ($nomecli && $endercli && $fonecli && $emailcli) {
-        mysqli_query($db, "INSERT INTO clientes (nomecli, endercli,fonecli,emailcli) VALUES ('$nomecli', '$endercli','$fonecli','$emailcli')");
+        
+    
+        #verifica se cliente id existe
+        $verificaCliente = mysqli_query($db, "SELECT idcli FROM clientes WHERE idcli = $idcli");
+        $vCli = mysqli_fetch_array($verificaCliente);
+        $vCli['idcli'] == $idcli ? $existCli = true : $existCli = false;
 
-        # grava mensagem na sessão
-        $_SESSION['message'] = "Cliente adicionado com sucesso!";
+        #verifica se existe produto id
+        $verificaProduto = mysqli_query($db, "SELECT id FROM produtos WHERE id=$idprod");
+        $vPro = mysqli_fetch_array($verificaProduto);
+        $vPro['id'] == $idprod ? $existProd = true : $existProd = false;
 
-        header('location: clientes.php');
+        #verifica se tem stock     
+        $verificaQtd  = mysqli_query($db, "SELECT p.qtdEstoque FROM produtos p WHERE (p.id = $idprod)");
+        while($vQtd = mysqli_fetch_array($verificaQtd)){
+            if ($vQtd['qtdEstoque']<$qtdven) {
+                $hasStock = false;
+            } else {
+                $hasStock = true;
+            }
+        }
+
+        // exbibe erros se houverem ou adiciona no banco caso contrario
+        if ($existCli == false) {
+            $_SESSION['message'] = "ID do cliente não foi encontrado no banco de dados!";
+            header('location: vendas.php');
+        } else if ($existProd == false) {
+            $_SESSION['message'] = "ID do Produto não foi encontrado no banco de dados!";
+            header('location: vendas.php');
+        } else if ($hasStock == false) {
+            $_SESSION['message'] = "Quantidade de vendas maior do que o estoque!";
+            header('location: vendas.php');
+        } else {
+            $mysql_result = mysqli_query($db, "INSERT INTO vendas (idcli,idprod,qtdven) VALUES ('$idcli', '$idprod','$qtdven')");
+            # grava mensagem na sessão
+            $_SESSION['message'] = "Venda cadastrada com sucesso!";
+            header('location: vendas.php');
+        }
     } else {
-        $_SESSION['message'] = "Preencha os campos corretamente! $nomecli,$endercli,$fonecli,$emailcli";
-
-
-        header('location: clientes.php');
+        $_SESSION['message'] = "Preencha os campos corretamente!";
+        header('location: vendas.php');
     }
 }
-
 # altera produto
 if (isset($_POST['altera'])) {
-    $idcli = $_POST['idcli'];
-    $nomecli = filter_input(INPUT_POST, 'nomecli');
-    $endercli = filter_input(INPUT_POST, 'endercli');
-    $fonecli = filter_input(INPUT_POST, 'fonecli');
-    $emailcli = filter_input(INPUT_POST, 'emailcli',FILTER_VALIDATE_EMAIL);
+    $codven = $_POST['codven'];
+    $idcli = filter_input(INPUT_POST, 'idcli', FILTER_VALIDATE_INT);
+    $idprod = filter_input(INPUT_POST, 'idprod', FILTER_VALIDATE_INT);
+    $qtdven = filter_input(INPUT_POST, 'qtdven', FILTER_VALIDATE_INT);
+    if ($idcli && $idprod && $qtdven) {
 
-    if ($nomecli && $endercli && $fonecli && $emailcli) {
-
-        $sql = "UPDATE clientes SET nomecli='$nomecli',endercli='$endercli',fonecli='$fonecli',emailcli='$emailcli' WHERE idcli = $idcli";
-        mysqli_query($db, $sql);
-        # grava mensagem na sessão
-        $_SESSION['message'] = "Cliente alterado com sucesso!";
-
-        header('location: clientes.php');
+         #verifica se tem stock     
+         $verificaQtd  = mysqli_query($db, "SELECT p.qtdEstoque FROM produtos p, vendas v WHERE (v.codven = $codven) AND(p.id = $idprod)");
+         $vQtd = mysqli_fetch_array($verificaQtd);
+         if ($vQtd['qtdEstoque']< $qtdven) {
+             $hasStock = false;
+         } else {
+             $hasStock = true;
+         }
+ 
+         #verifica se cliente id existe
+         $verificaCliente = mysqli_query($db, "SELECT idcli FROM clientes WHERE idcli = $idcli");
+         $vCli = mysqli_fetch_array($verificaCliente);
+         $vCli['idcli'] == $idcli ? $existCli = true : $existCli = false;
+ 
+         #verifica se existe produto id
+         $verificaProduto = mysqli_query($db, "SELECT id FROM produtos WHERE id=$idprod");
+         $vPro = mysqli_fetch_array($verificaProduto);
+         $vPro['id'] == $idprod ? $existProd = true : $existProd = false;
+ 
+ 
+         // exbibe erros se houverem ou adiciona no banco caso contrario
+         if ($existCli == false) {
+             $_SESSION['message'] = "ID do cliente não foi encontrado no banco de dados!";
+             header('location: vendas.php');
+         } else if ($existProd == false) {
+             $_SESSION['message'] = "ID do Produto não foi encontrado no banco de dados!";
+             header('location: vendas.php');
+         } else if ($hasStock == false) {
+             $_SESSION['message'] = "Quantidade de vendas maior do que o estoque!";
+             header('location: vendas.php');
+         } else {
+            $sql = "UPDATE vendas SET idcli='$idcli',idprod='$idprod',qtdven='$qtdven' WHERE codven=$codven";
+            mysqli_query($db, $sql);
+            # grava mensagem na sessão
+            $_SESSION['message'] = "Venda alterada com sucesso!";
+            header('location: vendas.php');
+         }
+       
     } else {
         $_SESSION['message'] = "Preencha os campos corretamente!";
 
 
-        header('location: clientes.php');
+        header('location: vendas.php');
     }
 }
 
 # remove produto
 if (isset($_GET['del'])) {
-    $idcli = $_GET['del'];
-    mysqli_query($db, "DELETE FROM clientes WHERE idcli=$idcli");
+    $codven = $_GET['del'];
+    mysqli_query($db, "DELETE FROM vendas WHERE codven=$codven");
 
     # grava mensagem na sessão
-    $_SESSION['message'] = "Cliente removido!";
-    header('location: clientes.php');
+    $_SESSION['message'] = "Venda removida!";
+    header('location: vendas.php');
 }
---
+++ vendas.php
@@ -0,0 +1,125 @@
+<?php include('crud_vendas.php'); ?>
+
+<?php
+# recupera o registro para edição
+if (isset($_GET['edit'])) {
+    $codven = $_GET['edit'];
+    $update = true;
+    $record = mysqli_query($db, "SELECT * FROM vendas WHERE codven=$codven");
+    # testa o retorno do select e cria o vetor com os registros trazidos
+
+    if ($record) {
+        $n = mysqli_fetch_array($record);
+        $codven = $n['codven'];
+        $idcli = $n['idcli'];
+        $idprod = $n['idprod'];
+        $qtdven = $n['qtdven'];
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Vendas</title>
+    <link rel="stylesheet" type="text/css" href="css/style_register.css">
+</head>
+
+<body>
+
+    <!-- teste se a sessão existe e exibe sua mensagem -->
+    <?php if (isset($_SESSION['message'])) : ?>
+        <div class="msg">
+            <?php
+            # exibe mensagem da sessão
+            echo $_SESSION['message'];
+            # apaga a sessão
+            unset($_SESSION['message']);
+            ?>
+        </div>
+    <?php endif ?>
+    <!-- ------------------------------------------------- -->
+
+    <!-- recupera os registros do banco de dados e exibe na página -->
+    <?php $results = mysqli_query($db, "SELECT V.codven, C.idcli, C.nomecli,P.id,P.nome, V.qtdven, V.qtdven * P.precoUnitario as valortotal from vendas as V
+inner join clientes as C
+on (V.idcli = C.idcli)
+inner join produtos as P
+on (P.id = V.idprod)"); ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Cod. Venda</th>
+                <th>Id Cliente</th>
+                <th>Nome Cliente</th>
+                <th>produto id</th>
+                <th>produto nome</th>
+                <th>Qtd Vendida</th>
+                <th>Valor Total</th>
+
+                <th colspan="4">Ação</th>
+            </tr>
+        </thead>
+        <!-- cria o vetor com os registros trazidos do select -->
+        <!-- Início while -->
+
+        <?php while ($rs = mysqli_fetch_array($results)) { ?>
+            <tr>
+                <td><?php echo $rs['codven']; ?></td>
+                <td><?php echo $rs['idcli']; ?></td>
+                <td><?php echo $rs['nomecli']; ?></td>
+                <td><?php echo $rs['id']; ?></td>
+                <td><?php echo $rs['nome']; ?></td>
+                <td><?php echo $rs['qtdven']; ?></td>
+                <td><?php echo $rs['valortotal']; ?></td>
+                <td>
+                    <!--produtos.php?edit = <php echo 1; ?>"-->
+                    <a href="vendas.php?edit=<?php echo $rs['codven']; ?>" class="edit_btn">Alterar</a>
+                </td>
+                <td>
+                    <a href="crud_vendas.php?del=<?php echo $rs['codven']; ?>" class="del_btn">Remover</a>
+                </td>
+            </tr>
+        <?php } ?>
+        <!-- Fim while -->
+    </table>
+    <!-- ------------------------------------------------------------ -->
+
+    <form method="post" action="crud_vendas.php">
+    
+        <input type="hidden" name="codven" value="<?php echo $codven; ?>">        
+        <div class="input-group">
+            <label>ID cliente:</label>
+            <!-- <input type="text" name="nome" value=""> -->
+            <input type="text" name="idcli" value="<?php echo $idcli; ?>">
+        </div>
+        <div class="input-group">
+            <label>ID produto:</label>
+            <!-- <input type="text" name="descricao" value=""> -->
+            <input type="text" name="idprod" value="<?php echo $idprod; ?>">
+        </div>
+        <div class="input-group">
+            <label>Qtd de Vendas:</label>
+            <!-- <input type="text" name="descricao" value=""> -->
+            <input type="text" name="qtdven" value="<?php echo $qtdven; ?>">
+        </div>
+
+        <div class="input-group">
+            <!-- <button class="btn" type="submit" name="adiciona">Adicionar</button> -->
+            <?php if ($update == true) : ?>
+                <button class="btn" type="submit" name="altera" style="background: #556B2F;">Alterar</button>
+            <?php else : ?>
+                <button class="btn" type="submit" name="adiciona">Adicionar</button>
+                <a href="close_connection.php" class="back_btn">Página Inicial</a>
+
+            <?php endif ?>
+
+        </div>
+    </form>
+</body>
+
+</html>
